42cf066a245503b35cb1e6c7b62a6540cb176bca
PracticalYT <37485242+PracticalYT@users.noreply.github.com>
2023-06-16 17:00:51 -0400

    Update config.lua

---
+++ KiloCoords/config.lua
@@ -0,0 +1,8 @@
+Config = Config or {}
+
+Config.EnableKeyMappings = false -- When you enable this, players will be able to change the keybind for this in their settings.
+Config.DefaultKeyMappings = {
+    ["vec2"] = "L",
+    ["vec3"] = "K",
+    ["vec4"] = "J"
+}
