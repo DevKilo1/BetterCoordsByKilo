cee94db27773f01a1f00de26741496c2b768d333
PracticalYT <37485242+PracticalYT@users.noreply.github.com>
2023-06-16 16:48:04 -0400

    .

---
+++ KiloCoords/client.lua
@@ -0,0 +1,83 @@
+ShowNotificationTicker = function(message)
+    BeginTextCommandDisplayHelp('STRING')
+    AddTextComponentSubstringPlayerName(message)
+    EndTextCommandDisplayHelp(0,0,1,-1)
+end
+
+
+RegisterCommand('vec2', function(source, args)
+    if args[1] == "cs" then
+        local coords = GetEntityCoords(PlayerPedId())
+
+        SendNUIMessage({
+            type = 'clipboard',
+            data = 'new Vector2(' .. coords.x.. 'f, '.. coords.y..'f)'
+        })
+    ShowNotificationTicker('Copied to clipboard! ' .. 'new Vector2(' .. coords.x.. 'f, '.. coords.y..'f)')
+    else
+        local coords = GetEntityCoords(PlayerPedId())
+        SendNUIMessage({
+            type = 'clipboard',
+            data = '' .. vec(coords.x, coords.y)
+        })
+        ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y))
+    end
+    
+end,true)
+
+RegisterCommand('vec3', function(source, args)
+    if args[1] == 'cs' then
+        local coords = GetEntityCoords(PlayerPedId())
+        SendNUIMessage({
+            type = 'clipboard',
+            data = 'new Vector3(' .. coords.x.. 'f, '.. coords.y..'f, '..coords.z..'f)'
+        })
+        ShowNotificationTicker('Copied to clipboard! ' .. 'new Vector3(' .. coords.x.. 'f, '.. coords.y..'f, '..coords.z..'f)')    
+    else
+        local coords = GetEntityCoords(PlayerPedId())
+        SendNUIMessage({
+            type = 'clipboard',
+            data = '' .. vec(coords.x, coords.y,coords.z)
+        })
+        ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y,coords.z))    
+    end
+    
+end,true)
+
+RegisterCommand('vec4', function(source, args)
+    if args[1] == 'cs' then
+        local coords, heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
+        SendNUIMessage({
+            type = 'clipboard',
+            data = 'new Vector4(' .. coords.x.. 'f, '.. coords.y..'f, '..coords.z..'f, '..heading..'f)'
+        })
+        ShowNotificationTicker('Copied to clipboard! ' .. 'new Vector4(' .. coords.x.. 'f, '.. coords.y..'f, '..coords.z..'f, '..heading..'f)')
+    else
+        local coords, heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
+        SendNUIMessage({
+            type = 'clipboard',
+            data = '' .. vec(coords.x, coords.y,coords.z,heading)
+        })
+        ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y,coords.z,heading))
+    end
+
+end,true)
+
+TriggerEvent('chat:addSuggestion', '/vec2', "Add 'cs' for the C# version. Example Usage:  '/vec2 cs'", {
+	{
+		name = "args",
+		help = "cs"
+	}
+})
+TriggerEvent('chat:addSuggestion', '/vec3', "Add 'cs' for the C# version. Example Usage:  '/vec3 cs'", {
+	{
+		name = "args",
+		help = "cs"
+	}
+})
+TriggerEvent('chat:addSuggestion', '/vec4', "Add 'cs' for the C# version. Example Usage:  '/vec4 cs'", {
+	{
+		name = "args",
+		help = "cs"
+	}
+})
---
+++ KiloCoords/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game { 'gta5' }
+
+author 'DevKilo, LVRP-BEN'
+
+description 'My own take at this resource. I had my grievances... Credits to LVRP-BEN for the copy-to-clipboard script (js and html)'
+
+files {
+    '*.*'
+}
+
+ui_page 'index.html'
+
+client_script 'client.lua'
